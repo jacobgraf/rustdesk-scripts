ccfda6adc2a30800a1ea1d55567d24439839abd6
Jacob Graf <jacobgraf@users.noreply.github.com>
2025-02-08 09:05:32 -0600

    Copy Scripts to Repo

---
+++ scripts/destiny/destiny-windows-cmd.cmd
@@ -0,0 +1,43 @@
+@echo off
+
+REM Assign the value random password to the password variable
+setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+set alfanum=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+set rustdesk_pw=
+for /L %%b in (1, 1, 32) do (
+    set /A rnd_num=!RANDOM! %% 62
+    for %%c in (!rnd_num!) do (
+        set rustdesk_pw=!rustdesk_pw!!alfanum:~%%c,1!
+    )
+)
+
+REM Get your config string from your Web portal and Fill Below
+set rustdesk_cfg="QfiIiOikXZrJCLiIiOikGchJCLiIiOikXYsVmciwiIt92YuEHamFmcn5SeulGdzVGZiojI0N3boJye"
+
+REM ############################### Please Do Not Edit Below This Line #########################################
+
+if not exist C:\Temp\ md C:\Temp\
+cd C:\Temp\
+
+curl -L "https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x86_64.exe" -o rustdesk.exe
+
+rustdesk.exe --silent-install
+timeout /t 20
+
+cd "C:\Program Files\RustDesk\"
+rustdesk.exe --install-service
+timeout /t 20
+
+for /f "delims=" %%i in ('rustdesk.exe --get-id ^| more') do set rustdesk_id=%%i
+
+rustdesk.exe --config %rustdesk_cfg%
+
+rustdesk.exe --password %rustdesk_pw%
+
+echo ...............................................
+REM Show the value of the ID Variable
+echo RustDesk ID: %rustdesk_id%
+
+REM Show the value of the Password Variable
+echo Password: %rustdesk_pw%
+echo ...............................................
